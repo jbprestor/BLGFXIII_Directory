8f72233797c6de7fd44f28e2dbf99c0c917a1f5a
unknown <jamesbryanprestor@gmail.com>
2025-09-03 13:23:52 +0800

    prepared for deploymenet

--- backend/src/blgfServer.js
+++ backend/src/blgfServer.js
@@ -4,44 +4,55 @@ import dotenv from "dotenv";
 import { routes as directoryRoutes } from "./routes/directoryRoutes.js";
 import { connectDB } from "./config/db.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+// Allow requests from your frontend or Postman (no origin)
 
 const CLIENT_API_URL =
   process.env.CLIENT_API_URL || process.env.VITE_CLIENT_API_URL_LOCAL;
 
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        try {
+          // Allow requests from your frontend or Postman (no origin)
+          if (!origin || origin === CLIENT_API_URL) {
+            callback(null, true); // allow
+          } else {
+            callback(new Error("Not allowed by CORS")); // reject
+          }
+        } catch (err) {
+          console.error("CORS error:", err);
+          callback(err); // pass error to Express
+        }
+      },
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+}
+
 //middleware
 app.use(express.json()); // this middleware will parse the JSON bodies: req.body
 app.use(rateLimiter);
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      try {
-        // Allow requests from your frontend or Postman (no origin)
-        if (!origin || origin === CLIENT_API_URL) {
-          callback(null, true); // allow
-        } else {
-          callback(new Error("Not allowed by CORS")); // reject
-        }
-      } catch (err) {
-        console.error("CORS error:", err);
-        callback(err); // pass error to Express
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
-//our simple custom middleware
-// app.use ((req,res,next) => {
-//     console.log("We just got a new req")
-//     next();
-// })
 
 app.use("/", directoryRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+//connect to db and start server
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
-let baseURL = "http://localhost:5001"; // default to local
-
-try {
-  // Optionally ping localhost here or just attempt request
-  // If fails, fallback to remote
-  baseURL = "http://localhost:5001";
-} catch {
-  baseURL = "https://cuddly-xylophone-wjxpw69g425pg7-5001.app.github.dev";
-}
+const BASE_URL = import.meta.env.MODE === "development"
+  ? import.meta.env.VITE_CLIENT_API_URL_LOCAL
+  : import.meta.env.VITE_CLIENT_API_URL;
 
 const api = axios.create({
-  baseURL,
+  baseURL: BASE_URL,
 });
 
 export default api;
