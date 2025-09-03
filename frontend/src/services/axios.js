41c13e0d681a6089851a1ac6e706340788b54986
Yudz <jamesbryanprestor@gmail.com>
2025-09-03 18:45:59 +0800

    Updated axios

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "production"
+const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001/api/directory/"
   : "api/directory";
 
