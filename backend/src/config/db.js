21269cb3d0ba82bd3f03d3a48cc1d001b7c64203
James Bryan <jamesbryanprestor@gmail.com>
2026-01-14 14:20:14 +0800

    fix(backend): improve db connection error handling for serverless

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,6 +6,6 @@ export const connectDB = async () => {
     // console.log("CONNECTED SUCCESSFULLY!");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
-    process.exit(1);
+    throw error;
   }
 };
